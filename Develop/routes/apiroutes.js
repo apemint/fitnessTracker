94093235446be496604471ea0111083181c2ec19
apemint <escobar.andres.alfonso@gmail.com>
2021-01-17 17:58:55 -0500

    server and html routes, api routes have problems, am fixing

--- Develop/routes/apiroutes.js
+++ Develop/routes/apiroutes.js
@@ -1,2 +1,52 @@
 const db = require("../models");
+const mongoose = require("mongoose");
+const mongojs = require("mongojs");
 
+module.exports = function (app) {
+
+    app.get("/api/workouts", (req, res) => {
+        db.Workouts.find({})
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.json(err);
+            });
+    });
+
+    app.put("/api/workouts/:id", (req, res) => {
+        db.Workouts.findOneAndUpdate(
+            { _id: req.params.id },
+            {
+                $inc: { totalDuration: req.body.duration },
+                $push: { workouts: req.body }
+            },
+            { new: true })
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.json(err);
+            });
+    });
+
+    app.post("/api/workouts", ({ body }, res) => {
+        db.Workouts.create(body)
+            .then((dbWorkout => {
+                res.json(dbWorkout);
+            })).catch(err => {
+                res.json(err);
+            });
+    });
+
+    app.get("/api/workouts/range", (req, res) => {
+        db.Workouts.find({})
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            }).catch(err => {
+                res.json(err);
+            });
+
+    });
+
+}
--- Develop/server.js
+++ Develop/server.js
@@ -1,32 +1,45 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
-//const seed = require("./seeders/seed")
-
-//port
 const PORT = process.env.PORT || 3000;
 
-//express app
-const app = express();
-
-const databaseUrl = "dbName";
-const collections = ["collectionNames"];
 
 const db = require("./models");
+const app = express();
 
-//log on db error
-// db.on("error", error => {
-//     console.log("Database Error:", error);
-//   });
+app.use(logger("dev"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    });
 
 //---------ROUTES------------
-require("./routes/html")(app);
-require("./routes/api")(app);
+require("./routes/htmlroutes")(app);
+require("./routes/apiroutes")(app);
 
 
 //-------app.listen----------
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
-  });
+});
+
+// db.Workout.deleteMany({})
+//   .then(() => db.Workout.collection.insertMany(workoutSeed))
+//   .then(data => {
+//     console.log(data.result.n + " records inserted!");
+//     process.exit(0);
+//   })
+//   .catch(err => {
+//     console.error(err);
+//     process.exit(1);
+//   });
